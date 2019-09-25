1ac201f46b96ec77c3d074ec135bd4832d490466
Solomon <solomon2.morrison@live.uwe.ac.uk>
2019-09-25 15:51:27 +0100

    #4 Added collision and kill zone
    Added  collision for the player and started on input and playermovement scripts. Killzone deletes player character and will lead to gameover screen

---
+++ Assets/Sol Assets/Scripts/Inputs.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inputs : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public Vector2 bothAxes;
+    public bool action;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        bothAxes = new Vector2(horizontalInput, verticalInput);
+
+    }
+}
