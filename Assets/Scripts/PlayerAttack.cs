547b8ef584683401c0b7cf27233e9dce9c05cdc9
Daniel Quarrell <dan.quarrell73@gmail.com>
2019-09-25 17:30:35 +0100

    #13 Player punch and web projectile implementation

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float attackCooldownDuration = 0.5f;
+    public GameObject webProjectile;
+    public Transform attackPosition;
+    public LayerMask enemyMask;
+    public float attackRange;
+    public int damageAmount;
+
+    float attackCooldownTimer = 0;
+
+    void Update()
+    {
+        if(attackCooldownTimer <= 0)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                if(Input.GetKey(KeyCode.S))
+                {
+                    WebProjectile projectile = Instantiate(webProjectile, attackPosition.position, Quaternion.identity).GetComponent<WebProjectile>();
+                    projectile.SpawnProjectile(new Vector2(3, 0), 0.25f);
+
+                    attackCooldownTimer = attackCooldownDuration;
+                }
+                else
+                {
+                    Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition.position, attackRange);
+                    for(int i = 0; i < enemiesToDamage.Length; i++)
+                    {
+                        if(enemiesToDamage[i].CompareTag("Enemy"))
+                        {
+                            enemiesToDamage[i].GetComponent<Health>().TakeDamage(damageAmount);
+                        }
+                    }
+                }
+
+                attackCooldownTimer = attackCooldownDuration;
+            }
+        }
+        else
+        {
+            attackCooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+    }
+}
---
+++ Assets/Scripts/WebProjectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebProjectile : MonoBehaviour
+{
+    Vector2 velocity = Vector2.zero;
+    float lifetime = 0;
+
+    public void SpawnProjectile(Vector2 _velocity, float _lifetime)
+    {
+        velocity = _velocity;
+        lifetime = _lifetime;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<Health>().TakeDamage(1);
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(velocity * Time.deltaTime);
+        
+        lifetime -= Time.deltaTime;
+        if(lifetime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
