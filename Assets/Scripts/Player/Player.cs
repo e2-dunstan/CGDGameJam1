464c5e6ccbf20df05a9edb5dde107bfc367e2a5b
Alex Allman <xalexxd@hotmail.co.uk>
2019-09-25 17:33:41 +0100

    #6 Added horizontal movement and jumping
    However jumping is currently broken due to a raycasting issue

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //TODO add extra references to player subsystems
+    [SerializeField] private PlayerMovement playerMovement = null;
+    public PlayerMovement PlayerMovement { get; private set; }
+
+    private static Player _instance = null;
+    public static Player Instance()
+    {
+        if(_instance == null)
+        {
+            _instance = new Player();
+        }
+        return _instance;
+    }
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,97 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float movementSpeed = 1f;
+    public float maxSpeed = 5f;
+    public float jumpVelocity = 1f;
+
+    private Rigidbody2D rigidbody = null;
+    private BoxCollider2D collider = null;
+    private float inputHorizontal = 0;
+    private float lastHorizontalInput = 0;
+    private bool isGrounded = true;
+    private bool triggerJump = false;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        isGrounded = CheckGrounded();
+        ApplyExternalForcesToVelocity();
+        HandlePlayerInput();
+        UpdateMovement();
+        UpdateJump();
+        UpdateLastHorizontalInput();
+    }
+
+    private bool CheckGrounded()
+    {
+        Vector2 raycastOrigin = new Vector2(collider.bounds.center.x, collider.bounds.min.y + 0.1f);
+        Debug.DrawRay(raycastOrigin, Vector2.down * 0.5f, Color.blue, 10);
+        RaycastHit2D rayHit = Physics2D.Raycast(raycastOrigin, Vector2.down, 0.5f);
+        return rayHit.collider.CompareTag("Ground");
+    }
+
+    private void HandlePlayerInput()
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        triggerJump = Input.GetButtonDown("Jump");
+    }
+
+    private void ApplyExternalForcesToVelocity()
+    {
+        Vector2 currentVel = rigidbody.velocity;
+        Vector2 newVel = Vector2.zero;
+
+        //Horizontal drag
+        float dragAmount = rigidbody.drag * Time.fixedDeltaTime;
+        float newXVel = currentVel.x < 0 ? currentVel.x + dragAmount : currentVel.x - dragAmount;
+        //Bitwise comparison to see if they are the same sign or if its gone from negative to possitive or visa versa then zero vel;
+        newVel.x = !((newXVel >= 0) ^ (currentVel.x < 0)) || currentVel.x == 0 ? 0 : newXVel;
+        //Cap the speed so it dont go crazy
+        if(newXVel > 0)
+        {
+            newVel.x = newXVel > maxSpeed ? maxSpeed : newXVel;
+        }
+        else if(newXVel < 0)
+        {
+            newVel.x = newXVel < -maxSpeed ? -maxSpeed : newXVel;
+        }
+
+        //Vertical drag / gravity
+        float gravityAmount = Physics2D.gravity.y * Time.fixedDeltaTime;
+        float newYVel = currentVel.y - gravityAmount;
+        newVel.y = CheckGrounded() ? 0 : newYVel;
+
+        rigidbody.velocity = newVel;
+    }
+
+    private void UpdateMovement()
+    {
+        Vector2 newVel = rigidbody.velocity;
+        //If the player switch direction then it zeros out the velocity to stop sliding
+        newVel.x = !((lastHorizontalInput >= 0) ^ (inputHorizontal < 0)) || inputHorizontal == 0 ? 0 : newVel.x;
+        newVel.x += inputHorizontal * movementSpeed * Time.fixedDeltaTime;
+        rigidbody.velocity = newVel;
+    }
+
+    private void UpdateJump()
+    {
+        if (triggerJump && isGrounded)
+        {
+            triggerJump = false;
+            Vector2 currentVel = rigidbody.velocity;
+            currentVel.y = jumpVelocity;
+            rigidbody.velocity = currentVel;
+        }
+    }
+
+    private void UpdateLastHorizontalInput()
+    {
+        lastHorizontalInput = Input.GetAxisRaw("Horizontal");
     }
 }
