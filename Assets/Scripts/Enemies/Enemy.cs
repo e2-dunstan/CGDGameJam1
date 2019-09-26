369d5ed70b32fd8706abebd320b0e22e3cb661a9
Ben Green <benjamin6.green@live.uwe.ac.uk>
2019-09-26 15:09:19 +0100

    #10 #8 Created base class for enemies and enemy movement.
    Currently CanSeePlayer function isn't detecting properly.

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum EnemyState
+    {
+        IDLE = 0,
+        WALKING = 1,
+        PERSUING = 2,
+        ATTACKING = 3,
+        STUNNED  = 4,
+        DYING = 5
+    }
+
+    [SerializeField] public EnemyState enemyState = EnemyState.IDLE;
+    [SerializeField] public int health {get; set;}
+
+    [SerializeField] protected bool canSeePlayer = false;
+    [SerializeField] protected float distanceFromPlayer;
+    [SerializeField] protected float visionRange = 1.0f;
+     
+    [SerializeField] protected bool isOnIdleCooldown = false;
+    [SerializeField] protected bool isOnAttackCooldown = false;
+    [SerializeField] protected float attackRange = 4.0f;
+    [SerializeField] protected float attackCooldown = 4.0f;
+
+    GameObject player;
+
+    protected void InflictDamage(int _damageAmount)
+    {
+        health = health - _damageAmount;
+    }
+
+    ///<summary> 
+    ///By default this will raycast out from the enemy for the specified visionRange. 
+    ///Override this if more complicated player detection is required.
+    ///</summary>
+    protected virtual bool CheckIfPlayerIsVisible(Vector2 _targetDestination)
+    {
+        canSeePlayer = false;
+
+        RaycastHit hit;
+
+        Physics.Raycast(transform.position, _targetDestination, out hit, visionRange);
+
+        Debug.DrawRay(transform.position, _targetDestination, Color.red);
+            
+
+        if (hit.transform != null && hit.transform.tag == "Player")
+        {
+            canSeePlayer = true;
+        }
+        else
+        {
+            canSeePlayer = false;
+        }
+
+        return canSeePlayer;
+    }
+
+    /// <summary>
+    /// Will calculateDistanceFromPlayer every update to determin the enemies current state
+    /// </summary>
+    protected virtual void CalculateDistanceFromPlayer()
+    {
+        //Will remove this gameObject find, using until this is merged with alex's work, then we're implementing something else
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        distanceFromPlayer = Vector2.Distance(gameObject.transform.position, player.transform.position);
+        
+    }
+}
---
+++ Assets/Scripts/Enemies/RhinoMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime;
+public class RhinoMovement : EnemyMovement
+{
+    [SerializeField] private float wonderingBoundaryXLeft = 1;
+    [SerializeField] private float wonderingBoundaryXRight= 1;
+
+    [SerializeField] private float wonderingBoundaryYUp= 1;
+    [SerializeField] private float wonderingBoundaryYDown = 1;
+    [SerializeField] private bool canFly = false;
+
+    [SerializeField] private bool hasRandomTargetLocation = false;
+
+    [SerializeField] private Vector2 randomLocation;
+
+    [SerializeField] private float randomXLocation;
+    [SerializeField] private float randomYLocation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomYLocation = gameObject.transform.position.y;
+        spawnPosition = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void MoveWithinDefinedWonderingBounds()
+    {
+        if (hasRandomTargetLocation == false)
+        { 
+            randomXLocation = Random.Range(spawnPosition.x - wonderingBoundaryXLeft, spawnPosition.x + wonderingBoundaryXRight);
+            
+            if (canFly == true)
+            {
+                 randomYLocation = Random.Range(spawnPosition.y - wonderingBoundaryYDown, spawnPosition.y + wonderingBoundaryYUp);
+            }
+
+            hasRandomTargetLocation = true;
+        }
+
+        //Check if unit is outside bounds
+        if(gameObject.transform.position.x < (spawnPosition.x - wonderingBoundaryXLeft) || gameObject.transform.position.x > (spawnPosition.x + wonderingBoundaryXRight) ||
+            gameObject.transform.position.y < (spawnPosition.y - wonderingBoundaryYDown) || gameObject.transform.position.y > (spawnPosition.y + wonderingBoundaryYUp))
+        {
+            hasRandomTargetLocation = false;
+        }
+
+        if(gameObject.transform.position != new Vector3(randomXLocation, randomYLocation, 0) && hasRandomTargetLocation == true)
+        {
+            targetDestination = new Vector2(randomXLocation, randomYLocation);
+            gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, new Vector2(randomXLocation, randomYLocation), moveSpeed);
+        }
+        else
+        {
+            hasRandomTargetLocation = false;
+        }
+
+    }
+
+    public override void MoveTowardsDestination(Vector2 _targetDestination)
+    {
+        targetDestination = _targetDestination;
+        gameObject.transform.position = Vector2.MoveTowards(gameObject.transform.position, _targetDestination, moveSpeed);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 0, 0.5f, 0.3f);
+        Gizmos.DrawCube(transform.position, new Vector3((wonderingBoundaryXLeft + wonderingBoundaryXRight), (wonderingBoundaryYDown + wonderingBoundaryYUp), 1));
+    }
+}
