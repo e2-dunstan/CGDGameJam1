1007d873a25ec94e203e2910b6791891ebdcd8ac
Ben Green <ben11344@msn.com>
2019-10-02 09:14:56 +0100

    #9 #8
    Push for lesson.
    Added some code for JSON loading / saving, but not currently usable.
    Defined class for saving score

---
+++ Assets/Scripts/SaveLoading/GameData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[System.Serializable]
+public class GameData : MonoBehaviour
+{
+    public struct LeaderboardEntry
+    {
+        public string playerName;
+        public int playerScore;
+    }
+
+    public List<EnemyMetaData> enemyMetaDataList = new List<EnemyMetaData>();
+
+    public List<LeaderboardEntry> leaderboardList = new List<LeaderboardEntry>();
+
+    public void AddScoreToLeaderboard(LeaderboardEntry _leaderboardEntry)
+    {
+        leaderboardList.Where(x => x.playerScore > _leaderboardEntry.playerScore);
+    }
+}
